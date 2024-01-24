8c3a2121581f26075410418d87ae928319dfab8a
JayPatel5999 <Jpatel9102@conestogac.on.ca>
2024-01-23 19:42:59 -0500

    Tweet Button is done. its opening new window and taking the quote to tweet

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ async function getQuote(url){
 }
 
 getQuote(api_url);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text="+quote.innerHTML+" "+"~"+author.innerHTML, "Tweet Window", "width=600, height=300");
+}
